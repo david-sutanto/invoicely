393a6c31479504db45c5b7820eb6a36311058c70
David Sutanto <hello.davidsutanto@gmail.com>
2025-02-07 18:49:07 +0800

    Create add new invoice page

---
+++ app/invoices/new/page.tsx
@@ -0,0 +1,50 @@
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import { sql } from "drizzle-orm";
+import { db } from "@/db";
+
+export default function CreateInvoice() {
+  return (
+    <main className="flex flex-col justify-center h-full gap-6 max-w-5xl mx-auto my-12">
+      <div className="justify-between flex"></div>
+      <h1 className="text-left font-bold">Invoices</h1>
+      <h1 className="text-left text-4xl font-bold">Create a New Invoice</h1>
+
+      <form className="gap-4 grid max-w-xs">
+        <div>
+          <Label htmlFor="name" className="block font-semibold text-sm mb-2">
+            Billing Name
+          </Label>
+          <Input id="name" name="name" type="text"></Input>
+        </div>
+        <div>
+          <Label htmlFor="email" className="block font-semibold text-sm mb-2">
+            Billing Email
+          </Label>
+          <Input id="email" name="email" type="text"></Input>
+        </div>
+        <div>
+          <Label htmlFor="value" className="block font-semibold text-sm mb-2">
+            Value
+          </Label>
+          <Input id="value" name="value" type="text"></Input>
+        </div>
+        <div>
+          <Label
+            htmlFor="description"
+            className="block font-semibold text-sm mb-2"
+          >
+            Description
+          </Label>
+          <Textarea id="description" name="description"></Textarea>
+        </div>
+        <div>
+          <Button className="w-full font-semibold">Submit</Button>
+        </div>
+      </form>
+    </main>
+  );
+}
