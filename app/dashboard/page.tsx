72100de56f594b2d09a2af7bdccd41b143f1dbb6
David Sutanto <hello.davidsutanto@gmail.com>
2025-02-07 18:08:01 +0800

    Create dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,65 @@
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import Image from "next/image";
+import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { CirclePlus } from "lucide-react";
+
+export default function Dashboard() {
+  return (
+    <main className="flex flex-col justify-center h-full text-center gap-6 max-w-5xl mx-auto my-12">
+      <div className="justify-between flex">
+        <h1 className="text-3xl font-bold">Invoices</h1>
+        <p>
+          <Button className="inline-flex gap-2" variant="ghost" asChild>
+            <Link href="/invoices/new">
+              <CirclePlus className="h-4 w-4" />
+              Create Invoice
+            </Link>
+          </Button>
+        </p>
+      </div>
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px] p-4">Date</TableHead>
+            <TableHead className="p-4">Customer</TableHead>
+            <TableHead className="p-4">Email</TableHead>
+            <TableHead className="text-center p-4">Status</TableHead>
+            <TableHead className="text-right p-4">Value</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium text-left p-4">
+              <span className="font-semibold">07/02/2025</span>
+            </TableCell>
+            <TableCell className="text-left p-4">
+              <span className="font-semibold">David Sutanto</span>
+            </TableCell>
+            <TableCell className="text-left p-4">
+              <span>david@davidsutanto.com</span>
+            </TableCell>
+            <TableCell className="text-center p-4">
+              <span className="font-semibold">
+                <Badge className="rounded-full">Open</Badge>
+              </span>
+            </TableCell>
+            <TableCell className="text-right p-4">
+              <span className="font-semibold">$250.00</span>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </main>
+  );
+}
